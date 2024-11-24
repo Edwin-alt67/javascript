1ba1d449cbb51779c1407fb2fd60b655c57b8520
edwin <edwincala0900@gmail.com>
2024-11-24 18:34:54 -0400

    añadiendo funciones de multiplicacion y division y llamando a dichas funciones para que el codigo funcione

--- calculadora.js
+++ calculadora.js
@@ -8,11 +8,23 @@ function calculadora(accion)
    {
      return 2+5;
    } 
-  function resta()
-    {
+   function resta()
+   {
       return 5-2;
-    }
+   }
+   //añadiendo codigo como contribuidor las funciones de multiplicacion y dificion
+   function multiplicacion()
+   {
+    return 5*2;
+   }
+   function division()
+   {
+    return 5/2;
+   }
 }
 
 console.log(calculadora('suma')); 
 console.log(calculadora('resta'));
+//llamando a las funciones
+console.log(calculadora('multiplicacion'));
+console.log(calculadora('division'));
